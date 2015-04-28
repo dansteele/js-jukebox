68bf7c8cb69dbffdd2042d59b6f1ae191be668a6
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-28 11:44:19 +0100

    Emulated rails root_to routing

---
+++ app/controllers/main.rb
@@ -0,0 +1,5 @@
+JsJukeboxV2::App.controller do
+  get "/" do
+    redirect "welcome"
+  end
+end
---
+++ app/controllers/welcome.rb
@@ -0,0 +1,7 @@
+JsJukeboxV2::App.controllers :welcome do
+  
+  get :index do
+    "Hello world"
+  end
+
+end
