7ecce813c2fa7fb506b6ce07e6f850cf814e7793
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-28 14:21:47 +0100

    Render soundcloud results

--- public/javascripts/soundcloud_search.js
+++ public/javascripts/soundcloud_search.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
-  $('#search_button').click(fetch_data)
-});
-
-function fetch_data() {
-  var param = $('#search_field').val()
   SC.initialize({
     client_id: '058ea073e0c993e2ef28ba095dca0647'
   });
+  $('#search_button').click(doSearch)
+});
 
-  function doSearch() {
-  var searchTerm = document.getElementById('search').value;
+
+function doSearch() {
+  var searchTerm = $('#search_field').val()
 
   // Encode spaces
   searchTerm = searchTerm.replace(" ", "+");
@@ -21,8 +19,12 @@ function fetch_data() {
         console.log("There are " + tracks.length + " results.")
         output += getHTML(track, tracks[track])
       }
-      document.getElementById("results").innerHTML = output
-    }
-  );
-  }; 
+      $("#search_results").hide().html(output).show()
+  });
+};
+
+function getHTML(index, data) {
+  html = '<li id="result"><a onclick="makeOverlay(self)" href="TARG"><h3>TITLE</h3><img src="IMG"/></a></li>'
+  html = html.replace("TARG", "#modal-container").replace("TITLE", data.title).replace("IMG", data.artwork_url)
+  return html
 }
