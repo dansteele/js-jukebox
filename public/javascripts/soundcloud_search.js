8724429cd41e68ff3b919803554209f744b080f5
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-28 16:28:16 +0100

    Render modal from SC object

--- public/javascripts/soundcloud_search.js
+++ public/javascripts/soundcloud_search.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  $('#modal').hide();
   SC.initialize({
     client_id: '058ea073e0c993e2ef28ba095dca0647'
   });
@@ -24,12 +25,16 @@ function doSearch() {
 };
 
 function getHTML(index, data) {
-  html = '<li id="SCID"><a onclick="makeOverlay(this.parentElement)" href="TARG"><h3>TITLE</h3><img src="IMG"/></a></li>'
-  html = html.replace("TARG", "#modal-container").replace("TITLE", data.title).replace("IMG", data.artwork_url).replace("SCID", data.id)
+  html = '<li id="SCID"><a onclick="makeOverlay(\'scURL\')" href="TARG"><h3>TITLE</h3><img src="IMG"/></a></li>'
+  html = html.replace("TARG", "#modal-container").replace("TITLE", data.title).replace("IMG", data.artwork_url).replace("scURL", data.permalink_url)
   return html
 }
 
 
 function makeOverlay(target) {
-  console.log($(target).attr('id'))
+  console.log(target)
+  $('#modal').show()
+  SC.oEmbed(target, { auto_play: true }, function(oEmbed) {
+    $('#modal').html(oEmbed.html);
+  });
 }
